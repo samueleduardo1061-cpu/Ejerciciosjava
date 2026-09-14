e56d97faed626f1dd78b3e91beea1f7973d3b35c
samueleduardo1061-cpu <samueleduardo1061@gmail.com>
2026-09-13 20:24:59 -0500

    primer ejercicio

---
+++ Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Inicialización directa del arreglo con los primeros 10 números primos
+        int[] primos = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+        // Imprimir el arreglo usando un bucle for-each
+        System.out.println("Los primeros 10 números primos son:");
+        for (int primo : primos) {
+            System.out.print(primo + " "); // ¡Corregido aquí!
+        }
+    }
+}
+
+    
